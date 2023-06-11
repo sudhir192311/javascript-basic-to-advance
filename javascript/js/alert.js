ee040adb0091129af492d27b9068115570c1185e
Sudhir swain <70561891+sudhir192311@users.noreply.github.com>
2023-06-11 21:44:01 +0530

    first commit

---
+++ javascript/js/alert.js
@@ -0,0 +1,18 @@
+var a = 12;
+var b = 24;
+/*var c =confirm("do u like our website")
+if(c){
+    alert("Thanks u")
+}
+else{
+    alert("Very sad for ur feedback");
+}
+*/
+var c = prompt("enter ur mark:")
+if(c>50 && c<101){
+    alert("u r are metrit")
+    
+} 
+else{
+    alert("sorry u r not merit")
+}
---
+++ javascript/js/function.js
@@ -0,0 +1,6 @@
+function sum(a,b){
+    var r = a + b;
+    return r;
+}
+var result = sum(1,3);
+document.write("sum of two no is: " +result);
